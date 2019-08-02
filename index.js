3d80bb8d5dbf979b8ea0a7fe2cf2a043cc3b8497
ethansagin <ethansagin@gmail.com>
2019-08-02 16:28:54 -0400

    Done.

--- index.js
+++ index.js
@@ -5,3 +5,35 @@ function preventRefreshOnSubmit(){
 }
 
 preventRefreshOnSubmit()
+
+const input = document.querySelector('input');
+
+function retrieveEmployeeInformation() {
+  return input.value;
+};
+
+function addNewElementAsLi() {
+  const newVal = retrieveEmployeeInformation();
+  const newLi = document.createElement('li');
+  newLi.innerHTML = newVal;
+  document.querySelector('.employee-list').appendChild(newLi);
+};
+
+function addNewLiOnClick() {
+  let submit = document.querySelector('input[type=submit]')
+  submit.addEventListener('click', function(e) {
+    addNewElementAsLi();
+    input.value = '';
+  });
+};
+
+function clearEmployeeListOnLinkClick() {
+  let link = document.querySelector('a')
+  let ul = document.querySelector('ul')
+
+  link.addEventListener('click', function(e) {
+    while(ul.firstChild) {
+      ul.removeChild(ul.firstChild);
+    };
+  });
+};
