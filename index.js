06097d32bf6aa9bf2f56a09988159aaa6d7c3429
Erin Stalzer <erinstalzer@gmail.com>
2019-08-02 15:10:56 -0400

    Done.

--- index.js
+++ index.js
@@ -12,16 +12,29 @@ function retrieveEmployeeInformation(){
 }
 
 function addNewElementAsLi(){
-  let li = document.createElement("LI")
-  li.innerHTML = retrieveEmployeeInformation()
-  document.getElementsByClassName("employee-list").appendChild(li);
+  let employeeName = retrieveEmployeeInformation()
+  let li = document.createElement("li")
+  li.innerHTML = employeeName
+  let ul = document.getElementsByClassName("employee-list")
+  ul[0].appendChild(li)
 }
 
 function addNewLiOnClick(){
-  return addNewElementAsLi()
+  addNewElementAsLi()
+  document.querySelector('form').reset()
 }
 
 function clearEmployeeListOnLinkClick(){
-  
+  let a = document.querySelector('a')
+  let ul = document.getElementsByClassName("employee-list")
+  // let childArr = document.querySelectorAll('li')
+  // for (let i = 0; i < childArr.length; i++){
+  //   parent[0].removeChild(childArr[i])
+  // }
+  a.addEventListener("click", function() {
+    while( ul[0].firstChild ){
+      ul[0].removeChild( ul[0].firstChild );
+    }
+  })
 }
 
